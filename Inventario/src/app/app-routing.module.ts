92573f8d46f0200de09281dd1fa3fe360f7dedd2
HITCARD\HitCard <l17091020@zacatepec.tecnm.mx>
2020-08-02 04:33:26 +0200

    Version 0.0.1 Falta por crear el menu desplegable en el home y arreglar lo de las tarjetas

---
+++ Inventario/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'metodopago',
+    loadChildren: () => import('./metodopago/metodopago.module').then( m => m.MetodopagoPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
