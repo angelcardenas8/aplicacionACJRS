7558204f38a475c3ebf88caba1dd7cadbfca2cc2
HITCARD\HitCard <l17091020@zacatepec.tecnm.mx>
2020-08-06 21:20:44 -0500

    Se hizo el login y register

--- Inventario/src/app/login/login.page.ts
+++ Inventario/src/app/login/login.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HomePage } from '../home/home.page';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
+ 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(public nav: NavController, private router: Router) { 
+    
+  
+
+  }
+
+
+  aqui(){
+
+  var container = document.querySelector(".container");
+  container.classList.add("sign-up-mode");
+    
+}  
+
+aqui2(){
+
+  var container = document.querySelector(".container");
+  container.classList.remove("sign-up-mode");
+    
+} 
+
+home(){
+  this.router.navigate(['/home']);
+}
+  
+  
 
   ngOnInit() {
+    
   }
 
 }
